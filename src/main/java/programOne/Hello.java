06ad5645a2c5d16151afd2d867d1b0836642eea1
Suganya0411 <149463817+Suganya0411@users.noreply.github.com>
2023-11-03 22:18:24 +0530

    add files

---
+++ src/main/java/programOne/Hello.java
@@ -0,0 +1,11 @@
+package programOne;
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println("Hello World");
+	System.out.println("Hello Wipro");
+	System.out.println("Hello Jenkins");
+	
+	
+}
+}
